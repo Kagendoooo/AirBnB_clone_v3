e95f95751e0b27f3592686fd24403b0653703478
Kamundia-Simon <simonkamundia8@gmail.com>
2024-08-06 16:57:59 +0000

    Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,13 @@ index view for the API.
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
@@ -13,3 +20,19 @@ def get_status():
     Returns the status of the API.
     """
     return jsonify(status='OK')
+
+
+@app_views.route('/stats')
+def get_stats():
+    """Get number of each object by type"""
+    stats = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
+    }
+    for key, value in stats.items():
+        stats[key] = storage.count(value)
+    return jsonify(stats)
