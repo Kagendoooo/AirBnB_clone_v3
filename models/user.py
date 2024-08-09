054fb3d2ea00c2a0a5c784bcb260f50dfd702663
Kamundia-Simon <simonkamundia8@gmail.com>
2024-08-09 22:23:59 +0000

    add md5 encryption

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ holds class User"""
+
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -27,3 +29,9 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, pwd, value):
+        """hash password using md5"""
+        if pwd == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(pwd, value)
